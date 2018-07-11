12464fe3630d7224286cb25e484aee0f58eee521
Amo Moloko <u14284783@tuks.co.za>
2018-07-11 15:41:56 +0200

    :dart:

--- src/index.js
+++ src/index.js
@@ -4,5 +4,31 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {BrowserRouter} from 'react-router-dom';
+
+
+const httpLink = new HttpLink({uri: 'http://localhost:4000'})
+
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+
+
+ReactDOM.render(
+    <BrowserRouter>
+         <ApolloProvider client={client}>
+       <App />
+        </ApolloProvider>
+</BrowserRouter>
+
+, document.getElementById('root'),);
+
 registerServiceWorker();
